97adacc88ea3e34d517519332242d602141b1b89
Ankita Kumari <ankita00iv@gmail.com>
2024-06-08 23:56:53 +0530

    initial commit

---
+++ RadixSort.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class RadixSort {
+
+    private static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int value : arr) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    private static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n]; 
+        int[] count = new int[10]; 
+
+        Arrays.fill(count, 0);
+        for (int value : arr) {
+            count[(value / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+  
+    public static void radixSort(int[] arr) {
+       
+        int max = getMax(arr);
+
+    
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    
+    private static void printArray(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        radixSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
